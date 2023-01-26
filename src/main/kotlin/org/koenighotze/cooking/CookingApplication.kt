8050500cfe0a4bd2e8a68001188edaf9bb179a06
David Schmitz <davidschmitz@posteo.de>
2023-01-26 14:58:22 +0100

    Import

---
+++ src/main/kotlin/org/koenighotze/cooking/CookingApplication.kt
@@ -0,0 +1,11 @@
+package org.koenighotze.cooking
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CookingApplication
+
+fun main(args: Array<String>) {
+	runApplication<CookingApplication>(*args)
+}
---
+++ src/test/kotlin/org/koenighotze/cooking/CookingApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.koenighotze.cooking
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CookingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
